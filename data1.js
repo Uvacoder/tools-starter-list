99eb539aa682ba50abc935e816b8fd774dbeec4c
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-01-17 03:31:32 -0500

    Create data1.js

---
+++ data1.js
@@ -0,0 +1,23 @@
+export const categories = ["Development", "Design", "Others", "Learning"];
+export const data = [
+  {
+    siteName: "Hoppscotch",
+    siteURL: "https://hoppscotch.io",
+    siteDesc: "An Open source API development ecosystem",
+    category: "Development",
+  },
+  {
+    siteName: "Sentry",
+    siteURL: "https://sentry.io",
+    siteDesc: 
+    "Application monitoring and error tracking software",
+    category: "Development",
+  },
+  {
+    siteName: "CodePen",
+    siteURL: "https://codepen.io/",
+    siteDesc:
+      "CodePen is a social development environment for front-end designers and developers",
+    category: "Development",
+  }
+]
